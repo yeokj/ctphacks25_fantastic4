2804568b7d30e8104b5a8562769dabba78f10e7b
Penny Wang <penny.ny.w@gmail.com>
2025-08-21 15:43:50 -0400

    fixed usemap

--- solar_housing_project/Solar_power/src/components/Map.jsx
+++ solar_housing_project/Solar_power/src/components/Map.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'; 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; 
 
 const UpdateMapView = ({ location }) => {
