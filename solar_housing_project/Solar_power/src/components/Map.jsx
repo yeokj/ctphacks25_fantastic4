56e901857b3bc7893fa5c13e9497dee8bacebc60
Penny Wang <penny.ny.w@gmail.com>
2025-08-21 10:58:35 -0400

    Add Map component to App.jsx

---
+++ solar_housing_project/Solar_power/src/components/Map.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; 
+
+const Map = () => {
+
+  const nycPosition = [40.7128, -74.0060];
+  
+  const zoomLevel = 7; 
+
+  return (
+    <MapContainer 
+      center={nycPosition} 
+      zoom={zoomLevel} 
+      scrollWheelZoom={false} 
+      style={{ height: '500px', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      
+      <Marker position={nycPosition}>
+        <Popup>
+          New York City
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
